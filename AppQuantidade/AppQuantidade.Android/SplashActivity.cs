0a0206714331084b8b2db10b40e4b88751dc9e0c
RD-Ricardo <ricardodias169@gmail.com>
2021-07-27 22:53:31 -0300

    Adicionar arquivos de projeto.

---
+++ AppQuantidade/AppQuantidade.Android/SplashActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppQuantidade.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", 
+        Label = "App Ceara", Icon = "@mipmap/ic_launcher", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | 
+        ConfigChanges.Orientation | ConfigChanges.UiMode | 
+        ConfigChanges.ScreenLayout | 
+        ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+
+            // Disable activity slide-in animation
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
---
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQuantidade
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantidade = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            quantidade++;
+            LblQuantidade.Text = quantidade.ToString(); 
+        }
+
+        private void Diminiur(object sender, EventArgs e)
+        {
+            if(quantidade != 0) 
+            {
+                quantidade--;
+                LblQuantidade.Text = quantidade.ToString();
+            }
+        }
+    }
+}
